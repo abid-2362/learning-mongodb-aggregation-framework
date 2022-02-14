fa156d6cd7a6038462ae05a4bc4e44603f8c119d
Abid Ali <abdsoftfsd@gmail.com>
2022-02-14 18:54:40 +0500

    Adds solution to lab 2.1 from the course website

--- lab2.1-cursor-like-stage.js
+++ lab2.1-cursor-like-stage.js
@@ -57,3 +57,40 @@ db.movies.aggregate([
     $limit: 1,
   }
 ]).pretty()
+
+// correct answer from course website
+db.movies.aggregate([
+  {
+    $match: {
+      "tomatoes.viewer.rating": { $gte: 3 },
+      countries: "USA",
+      cast: {
+        $in: favorites
+      }
+    }
+  },
+  {
+    $project: {
+      _id: 0,
+      title: 1,
+      "tomatoes.viewer.rating": 1,
+      num_favs: {
+        $size: {
+          $setIntersection: [
+            "$cast",
+            favorites
+          ]
+        }
+      }
+    }
+  },
+  {
+    $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 }
+  },
+  {
+    $skip: 24
+  },
+  {
+    $limit: 1
+  }
+])
